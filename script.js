f1fd004347c0128fb25c692556c622d5de73f3f3
subhadeep-IITISM <subhadeep@iitism.ac.in>
2026-08-07 19:43:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+function o(i){document.getElementById(i).style.display='block'}function c(i){document.getElementById(i).style.display='none'}function f(){let q=s.value.toLowerCase();document.querySelectorAll('.card').forEach(x=>x.style.display=x.dataset.search.includes(q)?'block':'none')}
